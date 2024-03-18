4ab167e4d285da9defa5376b9a8582081dac0758
simonericmoon <simon.dunheim@gmail.com>
2024-03-18 12:16:23 +0100

    Quartz sync: Mar 18, 2024, 12:16 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,12 +27,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
-  ],
-  right: [
     Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
   ],
+  right: [],  
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
